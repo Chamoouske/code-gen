9a98e06b7d9ec2a595366ff66d5ad134f5fd49bc
Ajax Lima <ajaxlima94@gmail.com>
2025-11-27 20:04:53 -0300

    feat: padronizacao de logs

---
+++ pkg/log/logger.go
@@ -0,0 +1,78 @@
+package logger
+
+import (
+	"context"
+	"log/slog"
+	"os"
+	"path/filepath"
+)
+
+type multiHandler struct {
+	handlers []slog.Handler
+}
+
+func GetLogger(name string) *slog.Logger {
+	return slog.Default().With("component", name)
+}
+
+func init() {
+	os.MkdirAll("./logs", 0o755)
+
+	consoleHandler := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo})
+
+	logPath := filepath.Join("./logs", "app.log")
+	logFile, err := os.OpenFile(logPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0o755)
+	if err != nil {
+		slog.Error("Erro ao abrir arquivo de log", "path", logPath, "error", err)
+		slog.SetDefault(slog.New(consoleHandler))
+		return
+	}
+
+	fileHandler := slog.NewTextHandler(logFile, &slog.HandlerOptions{
+		Level: slog.LevelInfo,
+	})
+
+	combinedHandler := newMultiHandler(fileHandler, consoleHandler)
+
+	slog.SetDefault(slog.New(combinedHandler))
+}
+
+func newMultiHandler(handlers ...slog.Handler) *multiHandler {
+	return &multiHandler{handlers: handlers}
+}
+
+func (h *multiHandler) Enabled(ctx context.Context, level slog.Level) bool {
+	for _, handler := range h.handlers {
+		if handler.Enabled(ctx, level) {
+			return true
+		}
+	}
+	return false
+}
+
+func (h *multiHandler) Handle(ctx context.Context, r slog.Record) error {
+	for _, handler := range h.handlers {
+		if handler.Enabled(ctx, r.Level) {
+			if err := handler.Handle(ctx, r); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func (h *multiHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
+	newHandlers := make([]slog.Handler, len(h.handlers))
+	for i, handler := range h.handlers {
+		newHandlers[i] = handler.WithAttrs(attrs)
+	}
+	return &multiHandler{handlers: newHandlers}
+}
+
+func (h *multiHandler) WithGroup(name string) slog.Handler {
+	newHandlers := make([]slog.Handler, len(h.handlers))
+	for i, handler := range h.handlers {
+		newHandlers[i] = handler.WithGroup(name)
+	}
+	return &multiHandler{handlers: newHandlers}
+}
